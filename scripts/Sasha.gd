a8549a6638571913f308a55e1c2b437ecf1d24e0
malexandra <6448982+Azralee@users.noreply.github.com>
2025-06-28 23:43:35 +0200

    Update Pathing and animation

--- scripts/Sasha.gd
+++ scripts/Sasha.gd
@@ -7,9 +7,8 @@ enum State { IDLE, FOLLOW }
 @onready var animatedSprite = $CollisionShape2D/AnimatedSprite2D
 @onready var astar_grid = AStarGrid2D.new()
 @export var movement_speed: float = 16.0
-@export var ARRIVE_DISTANCE = .02
+@export var ARRIVE_DISTANCE = .4
 @export var current_speed: Vector2 = Vector2(0., 0.)
-
 @onready var _tile_map = get_node("../TileMapLayer")
 
 var _state = State.IDLE
@@ -25,23 +24,30 @@ func _set_grid(active_grid: Rect2):
 	astar_grid.update()
 	
 func _ready() -> void:
-	set_movement_target(get_position())
-	
+	set_movement_target()
 	
 func _process(_delta):
 	if _state != State.FOLLOW:
 		return
 	var arrived_to_next_point = _move_to(_next_point)
 	if arrived_to_next_point:
+		_set_animation(true)
 		_path.remove_at(0)
 		if _path.is_empty():
 			_change_state(State.IDLE)
+			_set_animation()
 			return
 		_next_point = _path[0]
 
 func _move_to(local_position):
-	var _velocity = (local_position - position).normalized() * 16
-	position += _velocity * get_process_delta_time()
+	var _space_diff = local_position - position
+	_velocity = _snap_to_cardinal(_space_diff).normalized()
+	if (abs(_space_diff.length())<4):
+		_velocity = _space_diff.normalized()
+	_velocity *= 16. 
+	current_speed = _velocity
+	position += _velocity * get_process_delta_time() 
+	_set_animation()
 	return position.distance_to(local_position) < ARRIVE_DISTANCE
 
 func _unhandled_input(event):
@@ -53,12 +59,8 @@ func _unhandled_input(event):
 		elif event.is_action_pressed(&"move_to"):
 			_change_state(State.FOLLOW)
 
-func set_movement_target(_movement_target: Vector2):
-	if _state == State.IDLE:
-		movement_speed = 0.
-	else:
-		movement_speed = 16.
-
+func set_movement_target():
+	return
 
 func _on_velocity_computed(safe_velocity: Vector2):
 	velocity = safe_velocity
@@ -69,8 +71,12 @@ func _on_velocity_computed(safe_velocity: Vector2):
 func _change_state(new_state):
 	if new_state == State.IDLE:
 		_tile_map.clear_path()
+		set_movement_target()
+		_set_animation()
 	elif new_state == State.FOLLOW:
 		_path = _tile_map.find_path(position, _click_position)
+		set_movement_target()
+		_set_animation()
 		if _path.size() < 2:
 			_change_state(State.IDLE)
 			return
@@ -79,20 +85,33 @@ func _change_state(new_state):
 		_next_point = _path[1]
 	_state = new_state
 
+func _snap_to_cardinal(the_speed: Vector2) -> Vector2:
+	var unit_direction = the_speed.normalized()
+	var downness = unit_direction.dot(Vector2.DOWN)
+	var rightness = unit_direction.dot(Vector2.RIGHT)
+	if (abs(downness) > abs(rightness)):
+		if (downness > 0.):
+			return Vector2.DOWN
+		else:
+			return Vector2.UP
+	else:
+		if (rightness > 0.):
+			return Vector2.RIGHT
+		else:
+			return Vector2.LEFT
+	
 
-func _set_animation() -> void:
-	if (abs(current_speed.length()) < 16):
+func _set_animation(_force: bool = false) -> void:
+	if _state == State.IDLE:
 		animatedSprite.stop()
 		return
-	var unit_direction = current_speed.normalized()
-	var upness = unit_direction.dot(Vector2.UP)
-	var leftness = unit_direction.dot(Vector2.LEFT)
-	if (abs(upness) > abs(leftness)):
-		if (upness > 0.):
+	var unit_direction = _snap_to_cardinal(current_speed)
+	match unit_direction:
+		Vector2.UP:
 			animatedSprite.play("U")
-		else:
-			animatedSprite.play("U")
-	if (leftness > 0.):
+		Vector2.DOWN:
+			animatedSprite.play("D")
+		Vector2.LEFT:
 			animatedSprite.play("L")
-	else:
+		Vector2.RIGHT:
 			animatedSprite.play("R")
