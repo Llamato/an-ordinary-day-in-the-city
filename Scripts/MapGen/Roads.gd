2e30a09e37263c337f8f3935a7091507d3be7237
cedce01 <cedricgiese@gmail.com>
2025-09-24 22:30:07 +0200

    made the length of each road segment varaible

--- Scripts/MapGen/Roads.gd
+++ Scripts/MapGen/Roads.gd
@@ -22,7 +22,7 @@ class MazeRunner:
 
 func check_if_direction_is_open(pos: Vector2i, dir : Vector2i, length = 2) -> bool:
 	for i in range(1,length+1):
-		if get_cell_tile_data(pos + (dir * length)) != null:
+		if get_cell_tile_data(pos + (dir * i)) != null:
 			return false
 	return true
 
@@ -50,32 +50,37 @@ func generate_Map():
 	while stack.size() != 0 && maxRuntimeCounter > 0:
 		maxRuntimeCounter -= 1
 		print(maxRuntimeCounter)
-		
-		var currentRunner : MazeRunner = stack.pop_back() # might be interesting to choose a different strategy here. Even tho its less efficient ಥ_ಥ
-		
+		# might be interesting to choose a different strategy here. Even tho its less efficient ಥ_ಥ
+		#var currentRunner : MazeRunner = stack.pop_back()
+		var currentRunner : MazeRunner = stack.pop_at(rng.randi_range(0,stack.size()-1))
+		#Should always be a multiple of 2
+		var segmentLength = rng.randi_range(3,6) * 2
 		#Bounds Check
-		if currentRunner.pos.x > cityBoundsLeft && currentRunner.pos.x < cityBoundsRight:
-			if currentRunner.pos.y > cityBoundsUp && currentRunner.pos.y < cityBoundsDown:
+		if currentRunner.pos.x + segmentLength > cityBoundsLeft && currentRunner.pos.x - segmentLength < cityBoundsRight:
+			if currentRunner.pos.y + segmentLength> cityBoundsUp && currentRunner.pos.y - segmentLength < cityBoundsDown:
 				
 				var possibleDirections : Array[Vector2i] #Why do i have to use a dict godot ＞﹏＜
 				
-				if check_if_direction_is_open(currentRunner.pos, directions.left):
+				if check_if_direction_is_open(currentRunner.pos, directions.left, segmentLength):
 					possibleDirections.push_back(directions.left)
 				
-				if check_if_direction_is_open(currentRunner.pos, directions.right):
+				if check_if_direction_is_open(currentRunner.pos, directions.right, segmentLength):
 					possibleDirections.push_back(directions.right)
 				
-				if check_if_direction_is_open(currentRunner.pos, directions.up):
+				if check_if_direction_is_open(currentRunner.pos, directions.up, segmentLength):
 					possibleDirections.push_back(directions.up)
 				
-				if check_if_direction_is_open(currentRunner.pos, directions.down):
+				if check_if_direction_is_open(currentRunner.pos, directions.down, segmentLength):
 					possibleDirections.push_back(directions.down)
 				
 				if possibleDirections.size() > 0:
 					var picked : Vector2i = possibleDirections.pick_random()
-					draw_rectangle(currentRunner.pos, currentRunner.pos + picked * 2)
+					draw_rectangle(currentRunner.pos, currentRunner.pos + picked * segmentLength)
+					#Reminder Always depth first
+					#Otherwise you will run int some unsavory Images
 					stack.push_back(currentRunner)
-					stack.push_back(MazeRunner.new(currentRunner.pos + picked * 2))
+					stack.push_back(MazeRunner.new(currentRunner.pos + picked * segmentLength))
+					stack.push_back(MazeRunner.new(currentRunner.pos + picked * segmentLength))
 				
 
 func _init() -> void:
